bfd010bbe716b15b4a9c21b25b8cd1c6fb9e0d91
Mellart <artemmelashchnko@mail.ru>
2025-11-08 09:03:26 +0000

    Реализованная всплывающая форма задач

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,7 @@ import {ScilinkComponent} from "./scilink/scilink.component";
 import { CryptoRatesComponent } from './cryptorates/cryptorates.component'; 
 import {MplayerComponent} from "./mplayer/mplayer.component";
 import { KanbanComponent } from './kanban/kanban.component';
+import { TaskComponent } from './kanban/task/task.component';
 import { FileViewerComponent } from './file-viewer/file-viewer.component';
 import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component'
 import { WaterTrackerComponent } from './water-tracker/water-tracker.component';
@@ -199,6 +200,9 @@ const routes: Routes = [
   },
   { path: 'kanban',
     component: KanbanComponent
+  },
+  { path: 'kanban_task',
+    component: TaskComponent
   },
   {
     path: 'camera',
--- src/app/kanban/task/task.component.ts
+++ src/app/kanban/task/task.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss'],
+  standalone: true,
+  imports: [IonicModule, FormsModule]
 })
-export class TaskComponent  implements OnInit {
+export class TaskComponent {
+  task = {
+    id: 0,
+    title: '',
+    description: '',
+    priority: 'medium',
+    status: 'todo',
+    timeEstimate: 0,
+    link: ''
+  };
 
-  constructor() { }
+  constructor(private modalController: ModalController) {}
 
-  ngOnInit() {}
+  save() {
+    this.modalController.dismiss(this.task, 'save');
+  }
 
+  close() {
+    this.modalController.dismiss(null, 'cancel');
+  }
 }
